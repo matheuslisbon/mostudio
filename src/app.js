f4e9fb3d40f1617e4df41e52f0833a6d2dca8b85
matheus <matheuszicatop523@gmail.com>
2021-02-08 00:25:33 -0300

    adjuste port

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ app.use(express.json())
 app.use(express.static('public'));
 app.set('views', path.join(__dirname, "views"))
 app.set('view engine', 'ejs')
-const port = 3000 
+
 app.use(routes)
-app.listen(port, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
   console.log('START APP ON PORT 3000')
 })
